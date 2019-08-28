34e7495800fe3ee14dbc83e328e497fc6f175d56
Pierre Gabin FODOP GUMETE <pierre.fodop@ensta-bretagne.org>
2019-08-28 17:28:01 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ get 'easytt/index/:userid/:viewtype', :to => 'easytt#index'
 get 'easytt/index/:userid/:viewtype/:refdate', :to => 'easytt#index'
 
 get 'easytt/delete/:id', :to => 'easytt#delete'
+get 'easytt/drop_drag/:userid/:viewtype/:refdate/:id/:rdate',    :to => 'easytt#drop_drag'
 post 'easytt/edit/:userid/:viewtype/:refdate',  :to => 'easytt#edit'
 post 'easytt/create/:userid/:viewtype/:refdate',    :to => 'easytt#create'
 post 'easytt/multiple_create/:userid/:viewtype/:refdate',    :to => 'easytt#multiple_create'
